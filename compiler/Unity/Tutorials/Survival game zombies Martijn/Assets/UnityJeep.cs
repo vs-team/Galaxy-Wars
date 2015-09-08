3a2370ce67622a4868c7a929765df29aa89473ab
Marty1703 <martijndelange1703@gmail.com>
2015-09-08 12:55:03 +0200

    Walking zombies towards cube
    made today and yesterday

---
+++ compiler/Unity/Tutorials/Survival game zombies Martijn/Assets/UnityJeep.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityJeep : MonoBehaviour {
+
+    public static UnityJeep Instantiate()
+    {
+        var Jeep = GameObject.Instantiate(Resources.Load("Model/Jeep")) as GameObject;
+        var Jep = Jeep.GetComponent<UnityJeep>();
+        return Jep;
+    }
+
+    public Vector3 Position
+    {
+        get { return gameObject.transform.position; }
+        set { gameObject.transform.position = value; }
+    }
+    bool destroyed;
+    public bool Destroyed
+    {
+        get { return destroyed; }
+        set
+        {
+            destroyed = value;
+            if (destroyed)
+                GameObject.Destroy(gameObject);
+        }
+    }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
