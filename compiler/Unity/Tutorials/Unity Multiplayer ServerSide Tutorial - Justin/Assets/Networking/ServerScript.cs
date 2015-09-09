94ab71eb35390715de9b313aaa4d2bf20190b4a4
Justin van den Anker <justin.vanden.anker@gmail.com>
2015-09-09 16:03:24 +0200

    Updated Tutorial and Compiler for networking

--- compiler/Unity/Tutorials/Unity Multiplayer ServerSide Tutorial - Justin/Assets/Networking/ServerScript.cs
+++ compiler/Unity/Tutorials/Unity Multiplayer ServerSide Tutorial - Justin/Assets/Networking/ServerScript.cs
@@ -2,103 +2,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using Lidgren.Network;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class ServerScript : MonoBehaviour
 {
+  public BinaryFormatter bf;
+  public MemoryStream ms;
   public NetServer serv;
   public NetworkView networkView;
   public NetIncomingMessage inc;
   // Use this for initialization
 
-  public ServerScript Find()
+  public void Start()
   {
+      bf = new BinaryFormatter();
+      ms = new MemoryStream();
       GameObject var = GameObject.Find("Cube");
       var cube = GameObject.Find("Cube");
       var script = cube.GetComponent<ServerScript>();
       var test = new NetPeerConfiguration("ServerSide");
       test.LocalAddress = NetUtility.Resolve("localhost");
       test.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
-      test.Port = 57621;
+      test.MaximumConnections = 512;
+      test.Port = 443;
       serv = new NetServer(test);
       serv.Start();
       Debug.Log("Server is running!");
-      return script;
   }
   
 
   // Update is called once per frame
   void Update()
   {
-    if ((inc = serv.ReadMessage()) != null)
-    {
-        switch (inc.MessageType)
-        {
-            case NetIncomingMessageType.ConnectionApproval:
-                inc.SenderConnection.Approve();
-                break;
-            default:
-                Debug.Log("This type of message is not handled"+inc.MessageType);
-                Debug.Log(serv.ConnectionsCount);
-                break;
-        }
-    }
-    if(Input.GetKey(KeyCode.T))
-    {
-        serv.Shutdown("test");
-    }
+      if ((inc = serv.ReadMessage()) != null)
+      {
+          switch (inc.MessageType)
+          {
+              case NetIncomingMessageType.ConnectionApproval:
+                  inc.SenderConnection.Approve();
+                  Debug.Log("incoming");
+                  break;
+              case NetIncomingMessageType.Data:
+                  Debug.Log(inc.ReadString());
+                  var vec = 4.0f;
+                  bf.Serialize(ms, vec);
+                  byte[] byt = ms.GetBuffer();
+                  Debug.Log(byt);
+                  var resp = serv.CreateMessage();
+                  resp.Write(byt);
+                  inc.SenderConnection.SendMessage(resp, NetDeliveryMethod.ReliableOrdered, 3);
+                  break;
+              default:
+                  Debug.Log("This type of message is not handled" + inc.MessageType);
+                  Debug.Log(serv.ConnectionsCount);
+                  break;
+          }
+      }
 
-    if (Input.GetKeyDown(KeyCode.A))
-    {
-      networkView.RPC("MoveRight", RPCMode.All, null);
 
-    }
 
-    if (Input.GetKeyDown(KeyCode.D))
-    {
-      networkView.RPC("MoveLeft", RPCMode.All, null);
-    }
-
-    if (Input.GetKeyDown(KeyCode.X))
-    {
-      Network.InitializeServer(512, 25002, false);
-      Debug.Log("succesfully initialized server");
-    }
-
-    if (Input.GetKeyDown(KeyCode.Z))
-    {
-      Debug.Log("Trying to connect to server...");
-      Network.Connect("192.168.2.30", 25002);
-    }
-  }
-
-  void OnConnectedToServer()
-  {
-    Debug.Log("succesfully connected to Server");
-  }
-
-  void OnPlayerConnected()
-  {
-    Debug.Log("Player connected to server");
-  }
-
-  void OnDisconnectedFromServer()
-  {
-    Debug.Log("Disconnected from server");
-  }
-
-  void OnFailedToConnect(NetworkConnectionError n)
-  {
-    Debug.Log("Failed to connect" + n);
-  }
-
-  [RPC]
-  public void MoveLeft()
-  {
-    cube.transform.position -= Vector3.right * 2f;
-  }
-  [RPC]
-  public void MoveRight()
-  {
-    cube.transform.position += Vector3.right * 2f;
   }
 }
