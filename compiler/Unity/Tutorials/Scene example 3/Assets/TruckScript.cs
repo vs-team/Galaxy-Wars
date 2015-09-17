22a89c4fd91b779360ada877a347d49521cbf0e5
HBolijn <0877264@hro.nl>
2015-09-17 12:02:52 +0200

    Added half working jeep physics & rotation

--- compiler/Unity/Tutorials/Scene example 3/Assets/TruckScript.cs
+++ compiler/Unity/Tutorials/Scene example 3/Assets/TruckScript.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class TruckScript : MonoBehaviour {
 
+  public Rigidbody truckRigidBody;
 	public static TruckScript Instantiate()
     {
         GameObject var = GameObject.Find("truck");
-        return var.GetComponent<TruckScript>();
+        TruckScript truck = var.GetComponent<TruckScript>() as TruckScript;
+        truck.truckRigidBody = var.GetComponent<Rigidbody>() as Rigidbody;
+        return truck;
     }
 
     public Vector3 Position
     {
-        get {return gameObject.transform.position;}
-        set { gameObject.transform.position = value;}
+      get { return gameObject.transform.position; }
+      set { gameObject.transform.position = value; }
     }
 
     public Vector3 Rotation
@@ -20,4 +23,18 @@ public class TruckScript : MonoBehaviour {
         get { return gameObject.transform.eulerAngles; }
         set { gameObject.transform.eulerAngles = value;}
     }
-}                                                                                                                                                                                                                                                                                                              
+  
+    public float AddFors
+    {
+      set { truckRigidBody.AddForce(-transform.forward * value); }
+    }
+    public Vector3 RotateCar
+    {
+      set { truckRigidBody.AddRelativeTorque(value); }
+    }
+    public Vector3 CenterOfMass
+    {
+      get { return truckRigidBody.centerOfMass; }
+      set { truckRigidBody.centerOfMass = value; }
+    }
+}                                                                                                                         
