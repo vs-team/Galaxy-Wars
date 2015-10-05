4a06c5f50d4ce330c9cd1e706400d3e9da089f75
HBolijn <0877264@hro.nl>
2015-10-05 13:55:32 +0200

    Added gun&flashlight model

---
+++ compiler/Unity/Tutorials/Scene example flashlight/Assets/UnityCheckpoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityCheckpoint : MonoBehaviour {
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.name == "Jeep_KM_body")
+    {
+      Debug.Log("Entered");
+      privateIsEntered = true;
+    }
+  }
+
+
+
+  private bool privateIsEntered = false;
+  public bool isEntered
+  {
+    get { return privateIsEntered; }
+    set { privateIsEntered = value; }
+  }
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
