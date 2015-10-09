a1cfed77c09e41be86052b0f596ca2389e45cb37
Marty1703 <martijndelange1703@gmail.com>
2015-10-09 13:24:23 +0200

    pathfinding example

---
+++ compiler/Unity/Tutorials/pathfinding example/Assets/UnityCapsule.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityCapsule : MonoBehaviour {
+  public Transform goal;
+  public NavMeshAgent agent;
+  // Use this for initialization
+  void Start () {
+    goal = GameObject.FindWithTag("target").GetComponent<Transform>();
+    agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    agent.destination = goal.position;
+    goal.position += new Vector3(Input.GetAxis("Horizontal") * 0.2f,0.0f, Input.GetAxis("Vertical") * 0.2f);
+	}
+}
+                   
