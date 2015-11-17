43595ee077cf349af0edd41717fb45b297761989
Marty1703 <martijndelange1703@gmail.com>
2015-11-17 12:25:10 +0100

    Unity Explosion test

---
+++ compiler/Unity/Tutorials/Unity explosion/Assets/UnityBall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityBall : MonoBehaviour {
+  public GameObject ball;
+  public GameObject explosion;
+  void Start () {
+    ball = GameObject.Find("ball");
+    Debug.Log(ball.transform.position);
+    Instantiate();
+	}
+  public static UnityBall Instantiate()
+  {
+    GameObject ball = GameObject.Find("ball");
+    UnityBall a = ball.GetComponent<UnityBall>();
+    return a;
+  }
+
+  void Update () {
+  }
+  void OnCollisionEnter(Collision collision)
+  {
+    Debug.Log(collision.gameObject.name);
+    var x = collision.gameObject.transform.position;
+    Debug.Log(ball.transform.position);
+    GameObject expl = Instantiate(explosion, ball.transform.position, Quaternion.identity) as GameObject;
+    Destroy(ball);
+    Destroy(expl, 3.0f);
+  }
+
+}
+                      
